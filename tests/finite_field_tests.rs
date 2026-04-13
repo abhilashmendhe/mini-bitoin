c672e6959ea740d17215e05c9a352f0dc797bb13
Abhilash <abhilashmendhe@gmail.com>
2026-04-13 15:17:54 +0530

    condected a few tests on our finite fields

--- tests/finite_field_tests.rs
+++ tests/finite_field_tests.rs
@@ -1,4 +1,4 @@
-use mini_bitoin::finite_fields::field_element::FieldElement;
+use mini_bitoin::{finite_fields::field_element::FieldElement, utils::errors::BTCErr};
 
 #[test]
 fn check_finite_field_range()  {
@@ -25,4 +25,54 @@ fn check_finite_field_range()  {
         Err(err) => println!("❌ Error: {}",err),
     };
 
+}
+
+
+
+#[test]
+fn compare_finite_field() -> Result<(), BTCErr> {
+    let a = FieldElement::new(7, 13)?;
+    let b = FieldElement::new(6, 13)?;
+    assert!(a != b); 
+    Ok(())
+}
+
+#[test]
+fn add_finite_field() -> Result<(), BTCErr> {
+    let a = FieldElement::new(7, 13)?;
+    let b = FieldElement::new(12, 13)?;
+    let c = FieldElement::new(6, 13)?;
+    assert!((a+b)? == c);
+    Ok(())
+}
+
+#[test]
+fn sub_finite_field() -> Result<(), BTCErr> {
+    let a = FieldElement::new(7, 13)?;
+    let b = FieldElement::new(12, 13)?;
+    let c = FieldElement::new(8, 13)?;
+    assert!((a-b)? == c);
+    Ok(())
+}
+
+#[test]
+fn mul_finite_field() -> Result<(), BTCErr> {
+    let a = FieldElement::new(3, 13)?;
+    let b = FieldElement::new(12, 13)?;
+    let c = FieldElement::new(10, 13)?;
+    assert!((a*b)? == c);
+    Ok(())
+}
+
+#[test]
+fn exp_finite_field() -> Result<(), BTCErr> {
+    let f1 = FieldElement::new(2, 5)?;
+    let f2 = FieldElement::new(4, 5)?;
+    assert!(f1.pow_modulo(10) == f2);
+
+    let f3 = FieldElement::new(3, 23)?;
+    let f4 = FieldElement::new(2, 23)?;
+    assert!(f3.pow_modulo(29) == f4);
+
+    Ok(())
 }
