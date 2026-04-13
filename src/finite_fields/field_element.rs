5fcd735cefac3e04f29963a7ad240a802e027f29
Abhilash <abhilashmendhe@gmail.com>
2026-04-13 14:51:37 +0530

    Added add and sub implementation methods for finite field DS.

--- src/finite_fields/field_element.rs
+++ src/finite_fields/field_element.rs
@@ -1,4 +1,6 @@
-use crate::utils::errors::BTCErr;
+use std::ops::{Add, Sub};
+
+use crate::{finite_fields::modulo_helper::modulo, utils::errors::BTCErr};
 
 #[derive(PartialEq, PartialOrd, Eq, Clone)]
 pub struct FieldElement {
@@ -22,5 +24,42 @@ impl FieldElement {
 impl std::fmt::Display for FieldElement {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "FieldElement_{}({})", self.prime, self.num)
+    }
+}
+
+impl Add for FieldElement {
+    type Output = Result<FieldElement, BTCErr>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            return Err(BTCErr::TwoDiffFiniteFields("Addition".to_string()));
+        }
+        let num = modulo(modulo(self.num, self.prime) + modulo(rhs.num, rhs.prime), self.prime);
+        Ok(
+            FieldElement { 
+                num,
+                prime: self.prime
+            }
+        )
+    }
+}
+
+impl Sub for FieldElement {
+    type Output = Result<FieldElement, BTCErr>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            return Err(BTCErr::TwoDiffFiniteFields("Subtraction".to_string()));
+        }
+        let num = modulo(
+            modulo(self.num, self.prime) - modulo(rhs.num, rhs.prime) + self.prime, 
+            self.prime
+        );
+        Ok(
+            FieldElement { 
+                num,
+                prime: self.prime
+            }
+        )
     }
 }
