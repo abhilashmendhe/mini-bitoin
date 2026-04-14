acdb374815a7b0f4fadf71768c5c3277b856e17e
Abhilash <abhilashmendhe@gmail.com>
2026-04-14 08:51:31 +0530

    Refractor finite field methods. Added checked variants for safety

--- src/finite_fields/field_element.rs
+++ src/finite_fields/field_element.rs
@@ -2,14 +2,24 @@ use std::ops::{Add, Div, Mul, Sub};
 
 use crate::{finite_fields::modulo_helper::modulo, utils::errors::BTCErr};
 
-#[derive(PartialEq, PartialOrd, Eq, Clone)]
+#[derive(PartialEq, PartialOrd, Eq, Clone, Copy)]
 pub struct FieldElement {
     pub num: isize,
     pub prime: isize
 }
 
 impl FieldElement {
-    pub fn new(num: isize, prime: isize) -> Result<FieldElement, BTCErr> {
+
+    pub fn new(num: isize, prime: isize) -> Self {
+        assert!(prime > 1);
+        assert!(num < prime || num >= 0);
+        Self {
+            num,
+            prime, 
+        }
+    }
+
+    pub fn try_new(num: isize, prime: isize) -> Result<FieldElement, BTCErr> {
 
         if num >= prime || num < 0{
             return Err(BTCErr::FiniteFieldRangeErr { num, prime })
@@ -20,11 +30,39 @@ impl FieldElement {
         })
     }
 
-    pub fn pow_modulo(&self, n: isize) -> Result<FieldElement, BTCErr> {
+    pub fn checked_add(self, rhs: Self) -> Result<FieldElement, BTCErr> {
+        if self.prime != rhs.prime {
+            return Err(BTCErr::TwoDiffFiniteFields("Addition".to_string()));
+        }
+        Ok(self + rhs)
+    }
+
+    pub fn checked_sub(self, rhs: Self) -> Result<FieldElement, BTCErr> {
+        if self.prime != rhs.prime {
+            return Err(BTCErr::TwoDiffFiniteFields("Subtraction".to_string()));
+        }
+        Ok(self - rhs)
+    }
+
+    pub fn checked_mul(self, rhs: Self) -> Result<FieldElement, BTCErr> {
+        if self.prime != rhs.prime {
+            return Err(BTCErr::TwoDiffFiniteFields("Multiplication".to_string()));
+        }
+        Ok(self * rhs)
+    }
+
+    pub fn checked_div(self, rhs: Self) -> Result<FieldElement, BTCErr> {
+        if self.prime != rhs.prime {
+            return Err(BTCErr::TwoDiffFiniteFields("Division".to_string()));
+        }
+        Ok(self / rhs)
+    }
+
+    pub fn pow_modulo(&self, n: isize) -> FieldElement {
         
         if n < 0 {
-            let f1 = FieldElement::new(1, self.prime)?;
-            let f2 = FieldElement::new(self.num, self.prime)?.pow_modulo(-1*n)?;
+            let f1 = FieldElement::new(1, self.prime);
+            let f2 = FieldElement::new(self.num, self.prime).pow_modulo(-1*n);
             return f1 / f2;
         }
 
@@ -39,8 +77,10 @@ impl FieldElement {
             x = ((x % m) * (x % m)) % m;
             n /= 2;
         }
-        Ok(FieldElement { num: result, prime: self.prime })
+        FieldElement { num: result, prime: self.prime }
     }
+
+    // pub fn checked_pow_modulo(self)
 }
 
 impl std::fmt::Display for FieldElement {
@@ -50,51 +90,38 @@ impl std::fmt::Display for FieldElement {
 }
 
 impl Add for FieldElement {
-    type Output = Result<FieldElement, BTCErr>;
+    type Output = FieldElement;
 
     fn add(self, rhs: Self) -> Self::Output {
-        if self.prime != rhs.prime {
-            return Err(BTCErr::TwoDiffFiniteFields("Addition".to_string()));
+        let num = modulo(modulo(self.num, self.prime) + modulo(rhs.num, rhs.prime), self.prime);    
+        FieldElement { 
+            num,
+            prime: self.prime
         }
-        let num = modulo(modulo(self.num, self.prime) + modulo(rhs.num, rhs.prime), self.prime);
-        Ok(
-            FieldElement { 
-                num,
-                prime: self.prime
-            }
-        )
     }
 }
 
 impl Sub for FieldElement {
-    type Output = Result<FieldElement, BTCErr>;
+    type Output = FieldElement;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        if self.prime != rhs.prime {
-            return Err(BTCErr::TwoDiffFiniteFields("Subtraction".to_string()));
-        }
         let num = modulo(
             modulo(self.num, self.prime) - modulo(rhs.num, rhs.prime) + self.prime, 
             self.prime
-        );
-        Ok(
-            FieldElement { 
-                num,
-                prime: self.prime
-            }
-        )
+        );     
+        FieldElement { 
+            num,
+            prime: self.prime
+        }
     }
 }
 
 impl Mul for FieldElement {
-    type Output = Result<FieldElement, BTCErr>;
+    type Output = FieldElement;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        if self.prime != rhs.prime {
-            return Err(BTCErr::TwoDiffFiniteFields("Multiplication".to_string()));
-        }
         let num = modulo(modulo(self.num, self.prime) * modulo(rhs.num, rhs.prime), self.prime); 
-        Ok(FieldElement { num, prime: self.prime })
+        FieldElement { num, prime: self.prime }
     }
 }
 
@@ -115,13 +142,10 @@ impl Mul for FieldElement {
 */
 
 impl Div for FieldElement {
-    type Output = Result<FieldElement, BTCErr>;
+    type Output = FieldElement;
 
     fn div(self, rhs: Self) -> Self::Output {
-        if self.prime != rhs.prime {
-            return Err(BTCErr::TwoDiffFiniteFields("Division".to_string()));
-        }
-        let rhs_mul_inv = rhs.pow_modulo(rhs.prime - 2)?;
+        let rhs_mul_inv = rhs.pow_modulo(rhs.prime - 2);
         self * rhs_mul_inv
     }
 }
