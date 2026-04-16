09ea1abec5b428fac8fe100203092ba869a98d28
Abhilash <abhilashmendhe@gmail.com>
2026-04-16 14:43:18 +0530

    Updated mod.rs and hash_helper.rs func.

--- src/crypto/hash_helper.rs
+++ src/crypto/hash_helper.rs
@@ -1,16 +1,14 @@
-use num_bigint::BigInt;
-use num_traits::FromBytes;
+use num_bigint::{BigInt, Sign};
 use sha2::{Digest, Sha256};
 
 pub fn single_hash(data: String) -> BigInt {
     let data = data.as_bytes();
     let hash = Sha256::digest(data);
-    BigInt::from_be_bytes(&hash)
+    BigInt::from_bytes_be(Sign::Plus, &hash)
 }
-
 pub fn double_hash(data: String) -> BigInt {
     let data = data.as_bytes();
     let hash = Sha256::digest(data);
     let hash1 = Sha256::digest(&hash);
-    BigInt::from_be_bytes(&hash1)
+    BigInt::from_bytes_be(Sign::Plus, &hash1)
 }
--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -1,2 +1,6 @@
 pub mod s256_field;
 pub mod s256_point;
+pub mod signature;
+pub mod hash_helper;
+pub mod secret_field;
+pub mod private_key;
