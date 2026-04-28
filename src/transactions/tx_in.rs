18814138e9fa60d94945933b5940f4036f50ada3
Abhilash <abhilashmendhe@gmail.com>
2026-04-28 15:27:10 +0530

    Created additional struct for tx in and out.

--- src/transactions/tx_in.rs
+++ src/transactions/tx_in.rs
@@ -1,1 +1,29 @@
-pub struct TxIn;
+use crate::script::script::Script;
+
+pub struct TxIn {
+    pub prev_tx: Vec<u8>,
+    pub prev_ind: usize,
+    pub script_sig: Script,
+    pub sequence: u64,
+}
+
+impl TxIn {
+    pub fn new(
+        prev_tx: Vec<u8>,
+        prev_ind: usize,
+        script_sig: Option<Script>,
+        sequence: u64,
+    ) -> Self {
+        let script_sig = if let Some(sc) = script_sig {
+            sc
+        } else {
+            Script
+        };
+        Self {
+            prev_tx,
+            prev_ind,
+            script_sig,
+            sequence,
+        }
+    }
+}
--- src/transactions/tx_out.rs
+++ src/transactions/tx_out.rs
@@ -1,1 +1,16 @@
-pub struct TxOut;
+pub struct Satoshis(u64);
+
+impl Satoshis {
+    pub fn new(btc: f64) -> Self {
+        let amt = 100_000_000.0 * btc;
+        Self(amt as u64)
+    }
+    pub fn to_btc(&self) -> f64 {
+        self.0 as f64 / 100_000_000.0
+    }
+}
+
+pub struct TxOut {
+    pub satoshis: Satoshis,
+    pub script_pub_key: (),
+}
