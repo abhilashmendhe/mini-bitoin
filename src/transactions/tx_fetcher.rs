37e14be5e606cfbaed8b8e1e7d850c53d0086cba
Abhilash <abhilashmendhe@gmail.com>
2026-05-10 12:23:22 +0530

    completed tx fetcher

--- src/transactions/tx_fetcher.rs
+++ src/transactions/tx_fetcher.rs
@@ -22,7 +22,7 @@ const BLOCKSTREAM_TESTNET: &'static str = "https://blockstream.info/testnet/api"
 #[derive(Debug)]
 pub enum NET {
     MAINNET,
-    TESTNET
+    TESTNET,
 }
 
 #[derive(Debug)]
@@ -36,18 +36,27 @@ impl TxFetcher {
             cache: HashMap::new(),
         }
     }
-    pub fn fetch(&self, tx_id: String, testnet: bool, fresh: bool) -> Result<(), BTCErr> {
+    pub fn fetch(&mut self, tx_id: String, testnet: bool, fresh: bool) -> Result<Tx, BTCErr> {
         if fresh || !self.cache.contains_key(&tx_id) {
             let url = format!("{}/tx/{}/hex", get_url(testnet), tx_id);
             let response = reqwest::blocking::get(url).unwrap().text().unwrap();
-            
+
             let tx = Tx::parse(response)?;
-            println!("{}",tx_id);
-            println!("{}",tx.id()?);
-            
+            // println!("{}", tx_id);
+            // println!("{}", tx.id()?);
+
+            let net = if testnet { NET::TESTNET } else { NET::MAINNET };
+            self.cache.insert(tx_id.to_string(), (net, tx.clone()));
             // println!("{:?}", response);
+            if tx_id != tx.id()? {
+                return Err(BTCErr::DiffTxIds("Tx ids are different".to_string()));
+            }
         }
-        Ok(())
+        if let Some((_, tx)) = self.cache.get(&tx_id) {
+            Ok(tx.clone())
+        } else {
+            Err(BTCErr::TxNotFound(format!("Failed to fetch tx: {}", tx_id)))
+        }
     }
 }
 
