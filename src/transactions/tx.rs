eebd3b338ccda8ad6ce6ccb9de4e436cd428139a
Abhilash <abhilashmendhe@gmail.com>
2026-05-10 10:57:00 +0530

    Added witness in serialize method

--- src/transactions/tx.rs
+++ src/transactions/tx.rs
@@ -43,7 +43,7 @@ impl Tx {
     }
     
     pub fn hash(&self) -> Result<Vec<u8>, BTCErr> {
-        let mut h_ser = hash256(&self.serailize()?);
+        let mut h_ser = hash256(&self.serialize()?);
         h_ser.reverse();
         Ok(h_ser)
     }
@@ -75,6 +75,7 @@ impl Tx {
         let segwit = if &buffer[pos..pos + 2] == &[0, 1] {
             pos += 2;
             t_num_inputs = buffer[pos] as u64;
+            pos += 1;
             true
         } else {
             pos += 1;
@@ -88,8 +89,9 @@ impl Tx {
         // println!("current pos: {}", pos);
 
         let _tx_ins = TxIn::parse(t_num_inputs, &mut pos, &buffer)?;
+        // println!("\ntx_in: {:?}\n",_tx_ins);
         if segwit {
-            pos = pos + 1;
+            // pos = pos + 1;
         }
         // println!("current pos: {}", pos);
 
@@ -135,26 +137,37 @@ impl Tx {
         Ok(Tx::new(_version, segwit, _tx_ins, _tx_outs, witness, locktime, true))
     }
 
-    pub fn serailize(&self) -> Result<Vec<u8>, BTCErr> {
+    pub fn serialize(&self) -> Result<Vec<u8>, BTCErr> {
         let mut result = vec![];
         result.extend(self.version.to_le_bytes());
-        result.extend(script_encode_variant(self.tx_ins.len() as u64));
-        for tx_in in &self.tx_ins {
-            result.extend(tx_in.serialize()?);
-        }
 
         if self.segwit {
             result.extend(&[0, 1]);
         }
-
+        
+        result.extend(script_encode_variant(self.tx_ins.len() as u64));
+        for tx_in in &self.tx_ins {
+            result.extend(tx_in.serialize()?);
+        }
         result.extend(script_encode_variant(self.tx_outs.len() as u64));
         for tx_out in &self.tx_outs {
             result.extend(tx_out.serialize()?);
         }
 
         if self.segwit {
-            for v in &self.witness {
-                result.extend(v);
+            // println!("len of witness: {}", &self.witness.len());
+            let mut i = 0;
+            while i < self.witness.len() {
+                result.push(2);
+                let sig = &self.witness[i];
+                result.extend(script_encode_variant(sig.len() as u64));
+                result.extend(sig);
+                i += 1;
+
+                let pubkey = &self.witness[i];
+                result.extend(script_encode_variant(pubkey.len() as u64));
+                result.extend(pubkey);
+                i += 1;
             }
         }
 
