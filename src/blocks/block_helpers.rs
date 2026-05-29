494d9b9a3db9d123d0f392696a1a12ab61896ef9
Abhilash <abhilashmendhe@gmail.com>
2026-05-29 15:26:01 +0530

    added a bits_to_target in the helper file

---
+++ src/blocks/block_helpers.rs
@@ -0,0 +1,9 @@
+use num_bigint::BigInt;
+
+use crate::crypto::crypto_utils::little_endian_to_int;
+
+pub fn bits_to_target(bits: Vec<u8>) -> BigInt {
+    let exp = bits[bits.len()-1];
+    let coeff = little_endian_to_int(&bits[0..(bits.len()-1)]);
+    coeff * BigInt::from(256).pow((exp - 3) as u32)
+}
